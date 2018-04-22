2e6f82095f0feb260ffe5c8825dd5d5227091be3
mhr <mhr@users.noreply.github.com>
2018-04-21 20:19:28 -0400

    Update dependencies

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ setup(
       description='Sequence to Sequence Learning with Keras',
       author='Fariz Rahman',
       author_email='farizrahman4u@gmail.com',
-      url='https://github.com/farizrahman4u/seq2seq',
+      url='https://github.com/mhr/seq2seq',
       license='GNU GPL v2',
       install_requires=install_requires,
       packages=find_packages(),
-      dependency_links=['git+git://github.com/datalogai/recurrentshop.git']
+      dependency_links=['git+git://github.com/mhr/recurrentshop.git']
 )
